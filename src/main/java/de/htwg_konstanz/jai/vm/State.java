ca74c2ca6c75303fb2293b1677450df6446fd1cb
Oliver Haase <haase@htwg-konstanz.de>
2015-01-28 17:09:39 +0100

    Some code commented out to get compilable code.

--- src/main/java/de/htwg_konstanz/jai/vm/State.java
+++ src/main/java/de/htwg_konstanz/jai/vm/State.java
@@ -2,4 +2,19 @@ package de.htwg_konstanz.jai.vm;
 
 public final class State {
 
+	public Object getEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getOpStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getLocals() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
