8d5132779186f6e7b4a760441cda548ce40c92b0
Oliver Haase <haase@htwg-konstanz.de>
2015-01-29 12:46:27 +0100

    Continuous work on transfer functions on State and LocalVars level.

--- src/main/java/de/htwg_konstanz/jai/vm/State.java
+++ src/main/java/de/htwg_konstanz/jai/vm/State.java
@@ -2,17 +2,25 @@ package de.htwg_konstanz.jai.vm;
 
 public final class State {
 
+	public State(LocalVars localVars) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public State() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Object getEdges() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object getOpStack() {
+	public OpStack getOpStack() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object getLocals() {
+	public LocalVars getLocalVars() {
 		// TODO Auto-generated method stub
 		return null;
 	}
