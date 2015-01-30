c11f4fce49255efc3b431071e63c9f9abf771227
Oliver Haase <haase@htwg-konstanz.de>
2015-01-30 13:58:01 +0100

    Implementation for LocalVars.java added.

--- src/main/java/de/htwg_konstanz/jai/vm/LocalVars.java
+++ src/main/java/de/htwg_konstanz/jai/vm/LocalVars.java
@@ -1,10 +1,49 @@
 package de.htwg_konstanz.jai.vm;
 
-public final class LocalVars {
+import java.util.Arrays;
 
+import javax.annotation.CheckReturnValue;
+
+import lombok.EqualsAndHashCode;
+import net.jcip.annotations.Immutable;
+
+@Immutable
+@EqualsAndHashCode
+public class LocalVars {
+	private final Slot[] vars;
+
+	public LocalVars(int maxLocals) {
+		vars = new Slot[maxLocals];
+	}
+
+	// public LocalVars(@NonNull Slot[] vars) {
+	// this.vars = new Slot[vars.length];
+	//
+	// for (int i = 0; i < vars.length; i++)
+	// this.vars[i] = (vars[i] == null) ? null : vars[i].copy();
+	// }
+
+	private LocalVars(LocalVars original) {
+		vars = new Slot[original.vars.length];
+
+		for (int i = 0; i < vars.length; i++)
+			vars[i] = original.vars[i];
+	}
+
+	public Slot get(int index) {
+		return vars[index];
+	}
+
+	@CheckReturnValue
 	public LocalVars set(int index, Slot slot) {
-		// TODO Auto-generated method stub
-		return null;
+		LocalVars result = new LocalVars(this);
+		result.vars[index] = slot;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "L" + Arrays.toString(vars);
 	}
 
 }
