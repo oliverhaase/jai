f28b24aa384d98720d0a07f5010f5263b5c2ab90
Oliver Haase <haase@htwg-konstanz.de>
2015-02-04 17:16:05 +0100

    Component type changed from Slot to Type.

--- src/main/java/de/htwg_konstanz/jai/vm/OpStack.java
+++ src/main/java/de/htwg_konstanz/jai/vm/OpStack.java
@@ -1,8 +1,10 @@
 package de.htwg_konstanz.jai.vm;
 
+import de.htwg_konstanz.jai.gen.Type;
+
 public final class OpStack {
 
-	public Slot peek() {
+	public Type peek() {
 		// TODO Auto-generated method stub
 		return null;
 	}
