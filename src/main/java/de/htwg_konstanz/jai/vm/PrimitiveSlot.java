eb786818c728f308f28ca2a44cad9cd5352b50b2
Oliver Haase <haase@htwg-konstanz.de>
2015-02-04 17:14:35 +0100

    PrimitiveSlot made a singleton subclass of JastAdd node type Type.

--- src/main/java/de/htwg_konstanz/jai/vm/PrimitiveSlot.java
+++ src/main/java/de/htwg_konstanz/jai/vm/PrimitiveSlot.java
@@ -1,5 +1,14 @@
 package de.htwg_konstanz.jai.vm;
 
-public enum PrimitiveSlot implements Slot {
-	INSTANCE;
+import de.htwg_konstanz.jai.gen.Type;
+
+public class PrimitiveSlot extends Type {
+	private static PrimitiveSlot instance = new PrimitiveSlot();
+
+	private PrimitiveSlot() {
+	}
+
+	public static PrimitiveSlot getInstance() {
+		return instance;
+	}
 }
