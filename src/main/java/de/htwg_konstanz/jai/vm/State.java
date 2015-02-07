646f832c3f15822c50889998d6affa78bfc0fce1
Oliver Haase <haase@htwg-konstanz.de>
2015-02-07 15:27:47 +0100

    Aspect FieldEdges.jrag added as well as State.java and State.jrag adapted.

--- src/main/java/de/htwg_konstanz/jai/vm/State.java
+++ src/main/java/de/htwg_konstanz/jai/vm/State.java
@@ -3,10 +3,12 @@ package de.htwg_konstanz.jai.vm;
 public final class State {
 	private final LocalVars localVars;
 	private final OpStack opStack;
+	private final FieldEdges fieldEdges;
 
-	public State(LocalVars localVars, OpStack opStack) {
+	public State(LocalVars localVars, OpStack opStack, FieldEdges fieldEdges) {
 		this.localVars = localVars;
 		this.opStack = opStack;
+		this.fieldEdges = fieldEdges;
 	}
 
 	public OpStack getOpStack() {
@@ -15,6 +17,10 @@ public final class State {
 
 	public LocalVars getLocalVars() {
 		return localVars;
+	}
+
+	public FieldEdges getFieldEdges() {
+		return fieldEdges;
 	}
 
 }
